4414f24621dea4411129799a532c3154262c48b8
vietanhtbdh17 <vietanh160103@gmail.com>
2025-06-30 16:39:56 +0700

    sua app.js voi them component

---
+++ src/component/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class MyComponent extends React.Component {
+    //JSX
+    render() {
+        return (
+            <div>My first component
+                {Math.random()}
+            </div>
+        );
+    }
+}
+export default MyComponent;
