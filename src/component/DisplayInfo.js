36613489f1f46734938f6db32c6ac36f52f0d602
vietanhtbdh17 <vietanh160103@gmail.com>
2025-07-03 22:13:16 +0700

    Xoa class

--- src/component/DisplayInfo.js
+++ src/component/DisplayInfo.js
@@ -1,40 +1,7 @@
 import React, { useState } from 'react';
 import './DisplayInfo.scss';
 import logo from './../logo.svg';
-// import React, { useState } from 'react';
 
-// class DisplayInfo extends React.Component {
-
-//     render() {
-//         console.log(">>> Call render: 1");
-//         const { listUser } = this.props;
-//         // console.table(listUser);
-//         return (
-//             <div className="display-info-container">
-//                 {true &&
-//                     <div>
-//                         {listUser.map((user, index) => {
-//                             // console.log('>>> Check user: ', user);
-//                             return (
-//                                 <div key={user.id} className={+user.age > 18 ? 'green' : 'red'}>
-//                                     <div>
-//                                         <div>My name's {user.name}</div>
-//                                         <div>My age's {user.age}</div>
-//                                     </div>
-//                                     <div>
-//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-//                                     </div>
-//                                     <hr />
-//                                 </div>
-//                             )
-//                         })}
-
-//                     </div>
-//                 }
-//             </div>
-//         );
-//     }
-// }
 
 const DisplayInfo = (props) => {
     const { listUser } = props;
--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -3,51 +3,6 @@ import React, { useState } from 'react';
 import AddUserInfor from './AddUserInfor';
 import DisplayInfo from './DisplayInfo';
 
-// class MyComponent extends React.Component {
-//     state = {
-//         listUser: [
-//             { id: 1, name: 'Viet Anh 1', age: 15 },
-//             { id: 2, name: 'Viet Anh 2', age: 21 },
-//             { id: 3, name: 'Viet Anh 3', age: 30 }
-//         ]
-//     }
-
-//     handleAddNewUser = (userObj) => {
-//         console.log("Check :", userObj);
-//         this.setState({
-//             listUser: [userObj, ...this.state.listUser]
-//         });
-//     }
-//     handleDeleteUser = (userId) => {
-//         let listUserClone = this.state.listUser;
-//         listUserClone = listUserClone.filter(item => item.id !== userId);
-//         this.setState({
-//             listUser: listUserClone
-//         });
-//     }
-//     //JSX
-//     render() {
-
-//         return (
-//             <>
-//                 <div className='a'>
-//                     <AddUserInfor
-//                         handleAddNewUser={this.handleAddNewUser}
-//                     />
-//                     <br /> <br />
-//                     <DisplayInfo
-//                         listUser={this.state.listUser}
-//                         handleDeleteUser={this.handleDeleteUser}
-//                     />
-//                 </div>
-//                 <div className="b">
-
-//                 </div>
-//             </>
-
-//         );
-//     }
-// }
 
 
 
